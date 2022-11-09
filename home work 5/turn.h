72357e1d597dcb0510f4e2e5d6a98f62ae6ec1b1
fierteta1 <113540551+fierteta1@users.noreply.github.com>
2022-11-09 22:25:09 +0300

    Create turn.h

---
+++ home work 5/turn.h
@@ -0,0 +1,19 @@
+#ifndef TURN6P42
+#define TURN6P42
+#include <stdlib.h>
+
+typedef struct inode {
+    void *data;
+    struct inode *next;
+} inode;
+
+typedef inode *turn;
+
+void puttu(turn *t, void *data);
+
+void *gettu(turn *t);
+
+int sizetu(turn t);
+
+void cleartu(turn *t);
+#endif
