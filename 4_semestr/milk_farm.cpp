8c8d7c9b35b0c27de62e8019296a6bab8afc8f10
fierteta1 <113540551+fierteta1@users.noreply.github.com>
2023-05-17 02:10:30 +0300

    Update milk_farm.cpp

--- 4_semestr/milk_farm.cpp
+++ 4_semestr/milk_farm.cpp
@@ -1,74 +1,25 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-// Базовый класс для продуктов
-class Product {
-protected:
-    string name;     // Название продукта
-    double price;    // Цена продукта
-
-public:
-    Product(const string& name, double price)
-        : name(name), price(price) {}
-
-    double getPrice() const {
-        return price;
-    }
-
-    // Абстрактный метод для получения описания продукта
-    virtual string getDescription() const = 0;
-};
-
-// Класс для молока, наследуется от Product
-class Milk : public Product {
-public:
-    Milk(double price)
-        : Product("Молоко", price) {}
-
-    string getDescription() const override {
-        return "Бутылка молока";
-    }
-};
-
-// Класс для сыра, наследуется от Product
-class Cheese : public Product {
-public:
-    Cheese(double price)
-        : Product("Сыр", price) {}
-
-    string getDescription() const override {
-        return "Кусок сыра";
-    }
-};
-
-// Базовый класс для животных
+// Базовый класс Animal
 class Animal {
 protected:
-    string name;     // Имя животного
-    string gender;   // Пол животного
+    string name;
+    string gender;
 
 public:
     Animal(const string& name, const string& gender)
         : name(name), gender(gender) {}
 
-    // Абстрактный метод для издания звука
+    virtual ~Animal() {}
+
     virtual void produceSound() const = 0;
-
-    // Абстрактный метод для производства продукции
-    virtual void produceProduct(vector<Product*>& products) const = 0;
-
-    // Абстрактный метод для смерти животного
-    virtual void die() const = 0;
-
-    string getGender() const {
-        return gender;
-    }
 };
 
-// Класс для коровы, наследуется от Animal
+// Класс Cow, наследуется от Animal
 class Cow : public Animal {
 public:
     Cow(const string& name, const string& gender)
@@ -77,22 +28,9 @@ public:
     void produceSound() const override {
         cout << "Мууу!" << endl;
     }
-
-    void produceProduct(vector<Product*>& products) const override {
-        double milkPrice = 2.5;
-        if (gender == "женский") {
-            products.push_back(new Milk(milkPrice));
-            products.push_back(new Cheese(10.0));
-        }
-    }
-
-    void die() const override {
-        cout << "Корова " << name << " умерла." << endl;
-        delete this;
-    }
 };
 
-// Класс для козы, наследуется от Animal
+// Класс Goat, наследуется от Animal
 class Goat : public Animal {
 public:
     Goat(const string& name, const string& gender)
@@ -101,22 +39,50 @@ public:
     void produceSound() const override {
         cout << "Меее!" << endl;
     }
+};
 
-    void produceProduct(vector<Product*>& products) const override {
-        double milkPrice = 3.0;
-        if (gender == "женский") {
-            products.push_back(new Milk(milkPrice));
-            products.push_back(new Cheese(8.0));
-        }
-    }
+// Базовый класс Product
+class Product {
+protected:
+    string name;
+    double price;
 
-    void die() const override {
-        cout << "Коза " << name << " умерла." << endl;
-        delete this;
+public:
+    Product(const string& name, double price)
+        : name(name), price(price) {}
+
+    virtual ~Product() {}
+
+    virtual string getDescription() const = 0;
+
+    double getPrice() const {
+        return price;
     }
 };
 
-// Базовый класс для фабрик животных
+// Класс Milk, наследуется от Product
+class Milk : public Product {
+public:
+    Milk()
+        : Product("Молоко", 100.0) {}
+
+    string getDescription() const override {
+        return name;
+    }
+};
+
+// Класс Cheese, наследуется от Product
+class Cheese : public Product {
+public:
+    Cheese()
+        : Product("Сыр", 200.0) {}
+
+    string getDescription() const override {
+        return name;
+    }
+};
+
+// Базовый класс AnimalFactory
 class AnimalFactory {
 public:
     virtual Animal* createAnimal(const string& name, const string& gender) const = 0;
@@ -142,6 +108,7 @@ public:
 class Farm {
 private:
     vector<Animal*> animals;           // Вектор для хранения животных
+    vector<Product*> products;         // Вектор для хранения продукции
     AnimalFactory* animalFactory;      // Указатель на фабрику животных
 
     Farm() {
@@ -153,6 +120,9 @@ private:
         for (Animal* animal : animals) {
             delete animal;
         }
+        for (Product* product : products) {
+            delete product;
+        }
     }
 
 public:
@@ -163,107 +133,55 @@ public:
 
     // Установка фабрики животных
     void setAnimalFactory(AnimalFactory* factory) {
-        animalFactory = factory;
+        animalFactory= factory;
     }
 
-    // Добавление животного на ферму
-    Animal* addAnimal(const string& name, const string& gender) {
-        if (animalFactory != nullptr) {
-            Animal* newAnimal = animalFactory->createAnimal(name, gender);
-            animals.push_back(newAnimal);
-            return newAnimal;
-        }
-        return nullptr;
+// Добавление животного на ферму
+void addAnimal(const string& name, const string& gender) {
+    if (animalFactory != nullptr) {
+        Animal* animal = animalFactory->createAnimal(name, gender);
+        animals.push_back(animal);
     }
+}
 
-    // Продажа продукции
-    void sellProducts() const {
-        vector<Product*> products;
-
-        for (const Animal* animal : animals) {
-            animal->produceProduct(products);
+// Производство продукции
+void produceProducts() {
+    for (Animal* animal : animals) {
+        if (dynamic_cast<Cow*>(animal)) {
+            products.push_back(new Milk());
         }
-
-        double totalIncome = 0.0;
-
-        cout << "Продукция, доступная для продажи:" << endl;
-        for (const Product* product : products) {
-            cout << product->getDescription() << " - " << product->getPrice() << " рублей." << endl;
-            totalIncome += product->getPrice();
-        }
-
-        cout << "Общая прибыль от продажи: " << totalIncome << " рублей." << endl;
-
-        for (Product* product : products) {
-            delete product;
-        }
-
-        products.clear();
-    }
-
-    // Симуляция дня на ферме
-    void simulateDay() const {
-        for (const Animal* animal : animals) {
-            animal->produceSound();
+        else if (dynamic_cast<Goat*>(animal)) {
+            products.push_back(new Cheese());
         }
     }
+}
 
-    // Завершение дня на ферме
-    void endDay() const {
-        for (const Animal* animal : animals) {
-            animal->die();
-        }
-        animals.clear();
+// Продажа продукции
+void sellProducts() {
+    double totalRevenue = 0.0;
+    cout << "Продукция на ферме:" << endl;
+    for (Product* product : products) {
+        cout << product->getDescription() << " - " << product->getPrice() << " руб." << endl;
+        totalRevenue += product->getPrice();
     }
-};
-
-// Класс CowFactory, который реализует интерфейс IAnimalFactory и создает объекты Cow
-class CowFactory : public IAnimalFactory {
-public:
-    // Реализация метода создания животных
-    IAnimal* createAnimal(Gender gender) override {
-        return new Cow(gender);
-    }
-};
-
-// Класс GoatFactory, который реализует интерфейс IAnimalFactory и создает объекты Goat
-class GoatFactory : public IAnimalFactory {
-public:
-    // Реализация метода создания животных
-    IAnimal* createAnimal(Gender gender) override {
-        return new Goat(gender);
-    }
+    cout << "Общая выручка от продажи: " << totalRevenue << " руб." << endl;
+}
 };
 
 int main() {
-    // Создаем объект-синглтон фермы
     Farm& farm = Farm::getInstance();
+    farm.setAnimalFactory(new CowFactory());
+    farm.addAnimal("Буренка", "женский");
+    farm.addAnimal("Буренка2", "женский");
+    farm.addAnimal("Буренка3", "женский");
 
-    // Создаем объекты-фабрики для коров и козлов
-    CowFactory cowFactory;
-    GoatFactory goatFactory;
+    farm.setAnimalFactory(new GoatFactory());
 
-    // Добавляем на ферму несколько коров и козлов, используя фабричный метод
-    farm.addAnimal(cowFactory.createAnimal(Gender::Male));
-    farm.addAnimal(cowFactory.createAnimal(Gender::Female));
-    farm.addAnimal(cowFactory.createAnimal(Gender::Female));
-    farm.addAnimal(goatFactory.createAnimal(Gender::Male));
-    farm.addAnimal(goatFactory.createAnimal(Gender::Female));
+    farm.addAnimal("Козлик", "мужской");
+    farm.addAnimal("Козочка", "женский");
 
-    // Выводим информацию о всех животных на ферме
-    farm.printAnimalList();
-
-    // Производим продукцию и выводим информацию о собранном молоке и сыре
     farm.produceProducts();
-
-    // Продаем молоко и сыр и выводим информацию о выручке
     farm.sellProducts();
-
-    // Убираем животных с фермы, чтобы они не занимали место и ресурсы
-    farm.removeDeadAnimals();
-
-    // Выводим информацию о всех животных на ферме после уборки мертвых
-    farm.printAnimalList();
 
     return 0;
 }
